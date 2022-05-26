7f5f7eecc174090975e8e1278939c217ebc71b75
luiz.falavinha <luiz.falavinha@sharkdata.com.br>
2022-05-26 18:20:37 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,89 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly OpeningTimeOption _option;
+        public ProjectsController(IOptions<OpeningTimeOption> option)
+        {
+            _option = option.Value;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            return Ok(_option);
+        }
+
+        // api/projects/3
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title?.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction("GetById", new { id = createProject.Id }, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description?.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        // api/projects/3
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar, se não existir, retorna NotFound()
+
+            // Remover
+
+            return NoContent();
+        }
+
+        // api/projects/1/comments
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createComment)
+        {
+            return NoContent();
+        }
+
+        // api/projects/4/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            // iniciar projeto
+
+            return NoContent();
+        }
+
+        // api/projects/4/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            // finalizar projeto
+
+            return NoContent();
+        }
+    }
+}
---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // api/users/1
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        // api/users
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserModel createUserModel)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, createUserModel);
+        }
+
+        // api/users/login
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel login)
+        {
+            return Ok();
+        }
+    }
+}
